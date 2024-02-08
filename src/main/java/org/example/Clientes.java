1f54e047c794c8ddc6340e48087cb3e8b0d2cd3c
rent07 <140908065+rent07@users.noreply.github.com>
2024-02-07 22:03:05 -0600

    Creación de las clases Clientes y Productos

---
+++ src/main/java/org/example/Clientes.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Clientes {
+    int clave;
+    String nombre;
+}
---
+++ src/main/java/org/example/Productos.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Productos {
+    int clave;
+    String nombre;
+}
