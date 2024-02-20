af0fe8a50dbb30a80fa6277d94cd4b0ab16770f7
EmmanuelJuarezVelazquez <emmanuel-jv@hotmail.com>
2024-02-20 01:31:06 -0600

    Agregado de Form Clientes

---
+++ src/main/java/Forms/Clientes.java
@@ -0,0 +1,18 @@
+package Forms;
+
+import javax.swing.*;
+
+public class Clientes {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JRadioButton masculinoRadioButton;
+    private JRadioButton femeninoRadioButton;
+    private JRadioButton noDecirRadioButton;
+    private JSpinner spinner1;
+    private JButton aceptarButton;
+    private JButton cancelarButton;
+    private JButton salirButton;
+}
